709a9741ee7fb0e411e2974e27f66ddfac1623d7
Гусейнов Намик <gnamik@MacBook-Air-Gusejnov.local>
2024-04-28 21:48:44 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export const Button = () => {
+  return (
+    <View>
+      <Text>sgfdsf</Text>
+    </View>
+  );
+};
